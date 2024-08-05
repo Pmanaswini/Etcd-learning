331ce4aebc5cf8e82f2402a8f04c75d7b6fdbcc6
Piduguralla Manaswini <50841244+Pmanaswini@users.noreply.github.com>
2024-08-05 11:19:13 +0530

    Update etcdTest.cpp

--- etcdTest.cpp
+++ etcdTest.cpp
@@ -9,13 +9,9 @@ int main() {
 	etcd::Client etcd("http://127.0.0.1:2379");
 	// Set a key-value pair in etcd
 	etcd.set("/example_key", "example_value").wait();
-	std::cout<< "hello";
 	// get a key-value data in etcd
 	etcd::Response response = etcd.get("/example_key").get();
 	std::cout << response.value().as_string();
-	std::cout<< "hello";
-	
-	
 	return 0;
 }
 
